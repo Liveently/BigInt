07aaab24b8ebd1c168aab8ffeb7832a399be7fca
Marina Kosolap <70973716+Liveently@users.noreply.github.com>
2022-10-29 11:01:43 +0700

    Update bigint.cpp

--- bigint.cpp
+++ bigint.cpp
@@ -126,13 +126,14 @@ BigInt &BigInt::operator=(const BigInt &val) {
     if (&val == this) return *this;
 
     positive=val.positive;
+    digits=val.digits;
 
-    digits.clear();
+   /* digits.clear();
 
     for(int digit : val.digits){
         digits.push_back(digit);
     }
-
+*/
     return *this;
 }
 
@@ -298,15 +299,321 @@ bool BigInt::operator!=(const BigInt &val) const {
     return !(*this==val);
 }
 
+BigInt BigInt::operator+() const {
+    return *this;
+}
+
+BigInt BigInt::operator-() const {
+
+    BigInt res(*this);
+
+    if (digits.size()==1 && digits.back()==0) return res;
+
+    res.positive = !this->positive;
+
+    return res;
+}
+
+bool BigInt::operator>(const BigInt &val) const {
+
+    if (this==&val) return false;
+
+    if (this->positive != val.positive) return this->positive;
+
+    if(this->positive){
+        return module_compare(this->digits, val.digits);
+    } else{
+        return !module_compare(this->digits, val.digits);
+    }
+
+}
+
+bool BigInt::operator<(const BigInt &val) const {
+    return (this > &val);
+}
+
+bool BigInt::operator<=(const BigInt &val) const {
+    return !(this > &val);
+}
+
+bool BigInt::operator>=(const BigInt &val) const {
+    return ((this > &val) || (this==&val));
+}
+
+BigInt BigInt::operator~() const {
+
+    BigInt res;
+    res.positive= this->positive;
+
+    auto it= this->digits.rbegin();
+
+   // res.digits.push_back(~*it);
+
+    while(it!=digits.rend()){
+  //      int d = *it + base;
+
+     //   res.digits.push_back(~d);
+        res.digits.push_back(~*it);
+
+        it++;
+    }
+
+
+    return res;
+}
+
+BigInt operator*(const BigInt &a, const BigInt &b) {
+
+
+    std::vector<int>::const_iterator it1;
+    std::vector<int>::const_iterator it2;
+
+
+    BigInt c;
+
+    if ((a.digits.size()==1 && a.digits.back()==0) || (b.digits.size()==1 && b.digits.back()==0)){
+        c.digits.push_back(0);
+        return c;
+    }
+
+    c.positive=(a.positive==b.positive);
+
+    for (it1 = b.digits.begin(); it1 != a.digits.end(); ++it1) {
+        for (it2 = b.digits.begin(); it2 != b.digits.end(); ++it2) {
+            c.digits[distance(a.digits.begin(),it1)+ distance(a.digits.begin(),it2)] += (*it1) * (*it2);
+        }
+    }
+
+    return c;
+}
+
+BigInt &BigInt::operator*=(const BigInt &val) {
+    *this = *this * val;
+    return *this;
+}
+
+std::ostream &operator<<(std::ostream &o, const BigInt &i) {
+
+    if (i.digits.empty()) return o << 0;
+
+    if (i.digits.size() == 1 && i.digits.back() == 0) return o << 0;
+
+    if (!i.positive) o << '-';
+
+
+    auto it = i.digits.rbegin();
+
+    o << *it++;
+
+
+    for (; it != i.digits.rend(); ++it) {
+
+        int length = 0;
+
+
+        int current=BigInt::base;
+        while (current) {
+            current /= 10;
+            length++;
+        }
+
+        current = *it;
+
+        while (current) {
+            current /= 10;
+            --length;
+        }
+
+        for (int j = 0; j > length; ++j) o << '0';
+
+
+        if (*it) o << *it;
+    }
+
+    return o;
+
+}
+
+BigInt operator+(const BigInt &a, const BigInt &b) {
+    BigInt res(a);
+    res+=b;
+    return res;
+}
+
+BigInt operator-(const BigInt &a, const BigInt &b) {
+
+    BigInt res(a);
+    res-=b;
+    return res;
+}
+
+size_t BigInt::size() const {
+    return this->digits.size();
+}
+
+BigInt::operator int() const {
+
+    int res=0;
+    int k=1;
+
+    for(int digit : this->digits){
+        res+=digit*k;
+        k*=BigInt::base;
+    }
+
+    return res;
+}
+
+BigInt::operator std::string() const {
+
+    std::string o;
+
+    if (this->digits.size() == 1 && this->digits.back() == 0) return o = '0';
+
+    if (!this->positive) o.push_back('-');
+
+
+    auto it = this->digits.rbegin();
+
+
+    o+=std::to_string(abs(*it++));
 
 
 
+    for (; it != this->digits.rend(); ++it) {
+
+        int length = 0;
+
+        int current=BigInt::base;
+        while (current) {
+            current /= 10;
+            length++;
+        }
+
+        current = *it;
+
+        while (current) {
+            current /= 10;
+            --length;
+        }
+
+        for (int j = 0; j > length; ++j) o.push_back('0');
+
+        o+=std::to_string((abs(*it)));
+    }
+
+    return o;
+
+}
+
+BigInt operator/(const BigInt &a, const BigInt &b) {
+
+    BigInt c;
+
+    if((a.digits.size()==1 && a.digits.back()==0) || (a<b)){
+        c.digits.push_back(0);
+        return c;
+    }
+
+    c.positive=(a.positive==b.positive);
 
 
+    BigInt d(a);
 
+    while(module_compare(d.digits, b.digits)){
+        d-=b;
+        c++;
+    }
 
+    return c;
+}
 
+BigInt &BigInt::operator/=(const BigInt &val) {
+    *this = *this / val;
+    return *this;
+}
 
+BigInt operator^(const BigInt &a, const BigInt &b) {
+
+    BigInt c(1);
+
+    if(!a.positive && (b.digits[0] % 2 == 1)) c.positive = false;
+
+    BigInt d = b;
+
+    while(d.digits.size()==1 && d.digits.back()==0){
+        c=a*c;
+        d--;
+    }
+
+    return c;
+}
+
+BigInt &BigInt::operator^=(const BigInt &val) {
+    *this = *this ^ val;
+    return *this;
+}
+
+BigInt operator&(const BigInt &a, const BigInt &b) {
+
+    BigInt res;
+    res.positive= a.positive == b.positive;
+
+    auto it1= a.digits.rbegin();
+    auto it2= b.digits.rbegin();
+
+    while(it1!=a.digits.rend() && it2!=a.digits.rend()){
+
+        res.digits.push_back(*it1 & *it2);
+
+        it1++;
+        it2++;
+    }
+
+    return res;
+}
+
+BigInt &BigInt::operator&=(const BigInt &val) {
+    *this = *this & val;
+    return *this;
+}
+
+BigInt operator|(const BigInt &a, const BigInt &b) {
+    BigInt res;
+    res.positive= a.positive == b.positive;
+
+    auto it1= a.digits.rbegin();
+    auto it2= b.digits.rbegin();
+
+    while(it1!=a.digits.rend() || it2!=a.digits.rend()){
+
+        res.digits.push_back(*it1 || *it2);
+
+        it1++;
+        it2++;
+    }
+
+    return res;
+}
+
+BigInt &BigInt::operator|=(const BigInt &val) {
+    *this = *this | val;
+    return *this;
+}
+
+BigInt operator%(const BigInt &a, const BigInt &b) {
+
+    BigInt res(a);
+
+    res/=100;
+    res*=b;
+    return res;
+}
+
+BigInt &BigInt::operator%=(const BigInt &val) {
+    *this = *this % val;
+    return *this;
+}
 
 
 
